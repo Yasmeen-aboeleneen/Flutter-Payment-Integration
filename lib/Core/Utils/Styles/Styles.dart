5972f54795f81b698e81d4d3eb737994f9883e18
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-02-07 01:49:43 +0200

    Create App Styles

---
+++ lib/Core/Utils/Styles/Styles.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles{
+
+  static TextStyle style18=const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w400
+            );
+  static TextStyle styleBold18=const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w700
+            );
+
+  static TextStyle style20=const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w300
+            );          
+  static TextStyle style24=const TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.w600
+            );
+   
+ static TextStyle style25=const TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            );
+
+            
+            
+
+}
--- lib/Features/CheckOut/Presentation/Screens/MyCartScreenBody.dart
+++ lib/Features/CheckOut/Presentation/Screens/MyCartScreenBody.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_payments/Core/Utils/Styles/Styles.dart';
 
 class MyCartScreenBody extends StatelessWidget {
   const MyCartScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('My Cart',
+              textAlign: TextAlign.center, style: Styles.style25)),
+    );
   }
 }
