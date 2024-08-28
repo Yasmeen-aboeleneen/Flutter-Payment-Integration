596352f1f2a6c1956d749e6e41b83d1230cfd325
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-08-28 11:35:43 +0300

    Shaped Container

--- lib/Features/CheckOut/Presentation/Screens/LastView/last_Screen.dart
+++ lib/Features/CheckOut/Presentation/Screens/LastView/last_Screen.dart
@@ -7,6 +7,7 @@ class LastScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.brown,
       resizeToAvoidBottomInset: false,
       body: LastScreenBody(),
     );
--- lib/Features/CheckOut/Presentation/Screens/LastView/last_screen_body.dart
+++ lib/Features/CheckOut/Presentation/Screens/LastView/last_screen_body.dart
@@ -5,6 +5,30 @@ class LastScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Stack(children: [
+        Container(
+          width: 350,
+          height: 672,
+          decoration: ShapeDecoration(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20))),
+        ),
+        Positioned(
+            left: -20,
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.brown,
+            )),
+        Positioned(
+            right: -20,
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.brown,
+            ))
+      ]),
+    );
   }
 }
